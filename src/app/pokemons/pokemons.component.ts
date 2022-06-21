3d27a116bd2965d5f00725d4a21c1a6b76fe3ab2
C836 <gl.sepiol@gmail.com>
2022-06-21 10:34:50 -0300

    Exibindo resultados da API

--- src/app/pokemons/pokemons.component.ts
+++ src/app/pokemons/pokemons.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.services';
 
 @Component({
   selector: 'app-pokemons',
   templateUrl: './pokemons.component.html',
-  styleUrls: ['./pokemons.component.scss']
+  styleUrls: ['./pokemons.component.scss'],
 })
 export class PokemonsComponent implements OnInit {
+  pokemons: any[] = [];
 
-  constructor() { }
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
+    this.dataService.getPokemons().subscribe((response: any) => {
+      console.log(response)
+    });
   }
-
 }
