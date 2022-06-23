f3dd74d9f7551243de3c02a81232467f2bf54955
C836 <gl.sepiol@gmail.com>
2022-06-23 17:11:56 -0300

    FEAT: Implementando autocomplete na pesquisa

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,23 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.services';
+import * as pokemon from 'pokemon';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {
-  details:any = []
+export class SearchComponent implements OnInit {
+  details: any = [];
+  pokemons_list: readonly string[] = [];
 
   constructor(private dataService: DataService) {}
 
-  getPokemonData(name: any) {
-    if(name !== null){
-    this.dataService
-      .getPokemonData(name.value)
-      .subscribe((pokemonData: any) => {
-        this.details=(pokemonData)
-        console.log(pokemonData)
+  ngOnInit(): void {
+    this.pokemons_list = pokemon.all();
+  }
+
+  searchPokemon(input: HTMLInputElement, target: any) {
+    input.blur();
+    this.getPokemonData(target.value);
+  }
+
+  getPokemonData(name: string | undefined) {
+    if (name !== undefined) {
+      this.dataService.getPokemonData(name).subscribe((pokemonData: any) => {
+        this.details = pokemonData;
       });
     }
   }
