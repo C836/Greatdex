4f2970f11c7ccf064f9a741a1560d7df82a9cec2
C836 <gl.sepiol@gmail.com>
2022-06-23 12:18:04 -0300

    Atualizando nome e ícone do projeto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MarvelAPI'`, () => {
+  it(`should have as title 'PokeAPI'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MarvelAPI');
+    expect(app.title).toEqual('PokeAPI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MarvelAPI app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('PokeAPI app is running!');
   });
 });
