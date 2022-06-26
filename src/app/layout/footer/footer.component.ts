322379c11f30f84ecf3ae2a1827871d8742e2f17
C836 <gl.sepiol@gmail.com>
2022-06-26 17:56:41 -0300

    Espelhando estilo das bordas da página em um único arquivo

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./../borders.scss', './footer.component.scss']
 })
 export class FooterComponent implements OnInit {
 
