d11b8890603cc0dc2687fbc34487cd3c13cb4110
C836 <gl.sepiol@gmail.com>
2022-09-09 15:47:54 -0300

    chore: atualizando tipagem dos Cards

--- src/types/pokemons.ts
+++ src/types/pokemons.ts
@@ -1,4 +1,26 @@
 export interface PokemonConfig {
-    name: string;
-    sprites: any;
+  name: string;
+  sprites: any;
+  type: keyof typeof TypesConfig;
+}
+
+export enum TypesConfig {
+  "normal",
+  "fire",
+  "water",
+  "electric",
+  "grass",
+  "ice",
+  "fighting",
+  "poison",
+  "ground",
+  "flying",
+  "psychic",
+  "bug",
+  "rock",
+  "ghost",
+  "dragon",
+  "dark",
+  "steel",
+  "fairy",
 }
