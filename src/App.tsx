1d0b801a2a2decded46c34f77b70134dea7d66e5
C836 <gl.sepiol@gmail.com>
2022-09-09 15:49:36 -0300

    chore: atualizando módulo principal da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Pagination } from "./components/Pagination/Pagination";
+import { GlobalStyle } from "./global";
+import { Footer } from "./layout/footer/Footer";
+import { Header } from "./layout/header/Header";
+import { List } from "./views/PokemonList/List";
+
 function App() {
-  return <></>
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <List />
+      <Pagination page={0} />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
