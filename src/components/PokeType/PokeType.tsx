ab7e6828ebd193899ba3a042bb2bba7361e1b735
C836 <gl.sepiol@gmail.com>
2022-09-14 18:47:41 -0300

    chore: adicionando recebimento de método onClick ao componente Type

--- src/components/PokeType/PokeType.tsx
+++ src/components/PokeType/PokeType.tsx
@@ -1,9 +1,9 @@
 import * as S from "./PokeType.styled";
 import { TypesConfig } from "./../../types/pokemons"
 
-export function PokeType({type}:{type: keyof typeof TypesConfig}) {
+export function PokeType({type, onclick}:{type: keyof typeof TypesConfig, onclick?: React.MouseEventHandler<HTMLDivElement>}) {
   return (
-    <S.Type type={type}>
+    <S.Type type={type} title={type} onClick={onclick}>
       {type}
     </S.Type>
   );
